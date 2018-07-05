5e0821c132840bc0bb2fbd79807c953d9ec7e97e
Kurtis Gibb <kurtisgibb@outlook.com>
2018-07-05 13:10:49 -0400

    Loops Update

--- ScalaMachineLearning/src/basicconcepts/IfElse.scala
+++ ScalaMachineLearning/src/basicconcepts/IfElse.scala
@@ -1,12 +1,11 @@
 package basicconcepts
 
-object IfElse {
-  def main(args: Array[String]): Unit = {
+object IfElse extends App  {
     val x = true
      
     if(x == true){
       println("yes")
-    }
+    
   }
 }
 // The If.. Else expression is a classical programming construct for choosing a branch of code based on whether an expression resolves to true or false.
--- ScalaMachineLearning/src/basicconcepts/Sandbox.scala
+++ ScalaMachineLearning/src/basicconcepts/Sandbox.scala
@@ -1,10 +1,9 @@
 package basicconcepts
 
 object Sandbox {
-  def main(args: Array[String]): Unit = {
     val day = "Jupiter"
     val week = day match { case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" => "Weekday" case "Saturday" | "Sunday" => "Weekend" case _ => "not a day."}
     //'case _ => X' = Wildcard Operator; if the value does not correspond to match case, return 'X.'
     println(day + " is a " + week)
-  }
+  
 }
