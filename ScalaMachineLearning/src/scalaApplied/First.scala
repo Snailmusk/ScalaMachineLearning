37561a77a6bc4faf578f38c8957f282947ad8196
Aurora-2 <Aurora-2@10.1.65.36>
2018-07-30 11:18:42 -0500

    hjgf

--- ScalaMachineLearning/src/scalaApplied/First.scala
+++ ScalaMachineLearning/src/scalaApplied/First.scala
@@ -1,5 +1,4 @@
 package scalaApplied
-
 object First {
   //  elem(s: String): Element
   //
@@ -10,10 +9,25 @@ object First {
   // HIERARCHY: Array[String] -- ArrayElement --> Element <<abstract>> --> scala AnyRef <<java.lang.Object>>
   abstract class Element extends AnyRef {
     def contents: Array[String]
-    val height: Int = contents.length
-    val width: Int = if (height == 0) 0 else
+    def height: Int = contents.length
+    def width: Int = if (height == 0) 0 else
       contents.map(_.size).max
+    def example: Int = 42
+    final def example2: Int = 42
+    //final cannot be changed, overriden, subclass or extend down the hierarchy.
+
+    def above(that: Element): Element =
+      new ArrayElement(this.contents ++ that.contents)
+
+    def beside(that: Element): Element =
+      new ArrayElement(
+        for (
+          (line1, line2) <- this.contents zip that.contents
+        ) yield line1 + line2)
+    
+    override def toString = contents mkString "\n"
   }
+  
   /*Can call empty-paren methods with or without parens
    * Can only call parameterless methods without parens
    * (Recommended) Use parents if side effects
@@ -24,14 +38,38 @@ object First {
     //can't have field and method with same name
   }
 
+  class LineElement(s: String) extends ArrayElement(Array(s)) {
+    //LineElement is subclass of Element
+    //val can override def in subclass, but not in superclass
+    override def width = s.length
+    override val height = 1
+    override def example: Int = 1
+    //override... overrides.
+  }
+  
+  class UniformElement(
+      ch: Char,
+      override val width: Int,
+      override val height: Int
+      ) extends Element
+      {private val line = ch.toString * width
+      val contents = Array.fill(height)(line)
+      }
+  
   def main(args: Array[String]): Unit = {
 
     new ArrayElement(Array("hi"))
-    
+
     val ae = new ArrayElement(Array("hi", "planet"))
+    val le = new LineElement("Burp")
     println(ae.height)
     println(ae.width)
     println(ae.contents)
+    println(ae.example)
+    println(le.example)
+    println(ae.above(le))
+    println(ae.beside(le))
+    
 
   }
 }
--- ScalaMachineLearning/src/scalaApplied/ScalaTypeSystem.scala
+++ ScalaMachineLearning/src/scalaApplied/ScalaTypeSystem.scala
@@ -1,25 +1,15 @@
 package scalaApplied
 
 object ScalaTypeSystem {
-  /*
-  trait Food { val name: String }
-  trait Fruit extends Food
-  trait Cereal extends Food
-
-  case class Apple(name: String) extends Fruit
-  case class Orange(name: String) extends Fruit
-  case class Muesli(name: String) extends Cereal
-
-  case class FoodBowl[+F <: Food](food: F) {
-    def eat: String = s"Yummu ${food.name}"
-  }
-
-  def main(args: Array[String]): Unit = {
-    val fiji = Apple("fiji")
-    val jaffa = Orange("jaffa")
-    val alpen = Muesli("alpen")
-    //Type parameter becomes part of the overall Type; list of Int != list of Strings
-*/
+  /*FoodBowl[Apple] to be a sub-type of FoodBowl[Fruit] AND FoodBowl[Food]
+   * Relationship is known as variance and demarked by + and - on the generics
+   * +J means co-variant, -L means contra-variant, applied to generic class or trait
+   * if, neither means invariant.
+   * Invariant means no relation
+   * Covariant is typical variant order
+   * Contravariant is reverse direction hierarchy
+   * (Subtype/Supertype)
+   */
   abstract class Food { val name: String }
 
   abstract class Fruit extends Food
@@ -35,42 +25,54 @@ object ScalaTypeSystem {
     val name: String
     override def toString: String = s"Animal - $name"
   }
-  
+
+  case class Dog(name: String) extends Animal
+
   case class Bowl(food: Food) {
     override def toString = s"A bowl of yummy ${food.name}s"
     def contents = food
   }
-  
-  case class Dog(name: String) extends Animal
-  
+
   //GENERIC
-  case class Bowl2[J <: Food](contents: J) {
+  case class FoodBowl[+J <: Food](contents: J) {
     //<: is an upper bound, points along the hierarchy
     //type F, parameters will be F
+    //+ is allowing covariance
     override def toString: String = s"A yummy bowl of ${contents.name}s"
     //...yummy bowl of ${contents.name}s" won't work, not member of type parameter due to generic definition
   }
 
   def eat(f: Food): String = s"${f.name} eaten"
-  
+
+  def serveToFruitEater(fruitBowl: FoodBowl[Fruit]) =
+    s"Mmm, those ${fruitBowl.contents.name}s were very good"
+
+  def serveToFoodEater(foodBowl: FoodBowl[Food]) =
+    s"Mmm, those ${foodBowl.contents.name}s were very good"
+
   val fuji = Apple("Fuji")
   val alpen = Muesli("Alpen")
-  
-  val appleBowl = Bowl2(fuji)
-  val muesliBowl = Bowl2(alpen)
-  
-  //val numBowl = Bowl2(10)
-  //due to type parameterization of J being associated with Food, it can't be done
-  
-  val dottie = Dog("Dottie")
 
+  val appleBowl = FoodBowl(fuji)
+  val muesliBowl = FoodBowl(alpen)
+
+  //val numBowl = FoodBowl(10)
+  //due to type parameterization of J being associated with Food, it can't be done
+
+  val dottie = Dog("Dottie")
+  //val dogFoodBowl = Bowl(dottie) doesn't work, dottie isn't food
+
+  val fruitBowl = FoodBowl[Fruit](fuji)
+  val cerealBowl = FoodBowl[Cereal](alpen)
+  //type parameter is specified, Fruit. If remove, it would become a subtype Apple
   def main(args: Array[String]): Unit = {
 
     println(eat(fuji))
     println(Bowl(fuji))
-    
-    println(Bowl2(fuji))
-  
+    println(FoodBowl(fuji))
+    println(serveToFruitEater(fruitBowl))
+    //println(serveToFruitEater(cerealBowl)) doesn't work, as cereal is not fruit
+    println(serveToFoodEater(FoodBowl[Cereal](alpen)))
   }
 
 }
